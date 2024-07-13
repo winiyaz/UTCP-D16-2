f7cca91753f08b523ca77b0d8447363f43b81245
winiyaz <winiyaz@proton.me>
2024-07-12 20:20:00 -0700

    - Fixing all code here

--- 152/main.py
+++ 152/main.py
@@ -3,17 +3,36 @@
 This is similar to this
 https://github.com/K0unty/utcp1/blob/main/WORK/D15/D15.ipynb
 - This being converted into OOB
-
 '''
+
+from rich.prompt import Prompt as pro
+from rich import print as rprint
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-# Setting up the classes
+# Object instantiation
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
+menu = Menu()
+
 
 # Calling the reports
 coffee_maker.report()
 money_machine.report()
 
+# For running the while loop
+is_on = True
+
+while is_on:
+	options = menu.get_items()
+	choice = pro.ask(f'What you want? ({options})', default="off")
+	if choice == 'off':
+		is_on = False
+	elif choice == 'report':
+		coffee_maker.report()
+		money_machine.report()
+	else:
+		drink = menu.find_drink(choice)
+		if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+			coffee_maker.make_coffee(drink)
