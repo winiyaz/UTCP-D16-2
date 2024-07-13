9067c0b34314c89d2346e14fbbd4e25f1059940f
winiyaz <winiyaz@proton.me>
2024-07-12 18:14:18 -0700

    PretyTablTest - Testing Colors

--- PrettyTable.py
+++ PrettyTable.py
@@ -1,5 +1,30 @@
 # File for testin pretty table
 # use Ctrl+Shift+F10 to run this
 
-import prettytable
 
+from rich import print as rprint
+from rich.pretty import pprint
+from rich import inspect
+from rich.console import Console
+from rich.markdown import Markdown
+from rich.panel import Panel
+from rich import box
+from rich.prompt import Prompt
+from rich.style import Style
+from rich.text import Text
+console = Console()
+from rich.traceback import install
+install(show_locals=True)
+
+#------------------
+
+from prettytable import PrettyTable
+table = PrettyTable()
+inspect(table, methods=True)
+
+rprint(f'''
+[red] Whatup Whatup Whatup Whatup Whatup
+Whatup Whatup Whatup Whatup Whatup
+Whatup Whatup Whatup Whatup Whatup
+Whatup Whatup Whatup Whatup Whatup
+''')
