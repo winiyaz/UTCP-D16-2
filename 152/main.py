64b253386eb51aad30cf3250df4c8e0f5ae74e19
winiyaz <winiyaz@proton.me>
2024-07-12 19:43:12 -0700

    Start Project

--- 152/main.py
+++ 152/main.py
@@ -1,5 +1,14 @@
-# Testing nested Main.py
+# Section 152
 
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+# Setting up the classes
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+
+# Calling the reports
+coffee_maker.report()
+money_machine.report()
+
